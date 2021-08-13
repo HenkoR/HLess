339575613a07dac2110dd29c1b35c7ab2f4b74b7
HenkoR <henkorabie99@gmail.com>
2021-08-13 15:49:50 +0200

    Adding dto model

---
+++ src/Hless.Api/Models/Dto/SchemaDto.cs
@@ -0,0 +1,12 @@
+namespace Hless.Api.Models.Dto
+{
+    public record SchemaDto
+    {
+        public long SchemaId { get; init; }
+        public string Name { get; init; }
+        public string Definition { get; init; }
+        public string DraftDefinition { get; init; }
+        public string CreatedBy { get; init; }
+
+    }
+}
