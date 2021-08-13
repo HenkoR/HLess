597201a24e82a038823283dd89936bd23204475b
HenkoR <henkorabie99@gmail.com>
2021-08-13 15:47:46 +0200

    Adding Common shared library for repository interfaces

---
+++ src/Hless.Common/Repositories/ISchemaRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hless.Data.Models;
+
+namespace Hless.Common.Repositories 
+{
+    public interface ISchemaRepository
+    {
+        Task<IEnumerable<Schema>> GetSchemasAsync(); 
+        Task<Schema> GetSchemaAsync(long schemaId);
+        Task CreateSchemaAsync(Schema schema);
+        Task UpdateSchemaAsync(Schema schema);
+        Task PublishSchemaAsync(long schemaId);
+    }
+}
