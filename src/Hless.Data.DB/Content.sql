7fb0a50e51b87d571f38b60ed49281092a27a847
Henko Rabie <henko@rabiekonsulting.com>
2021-09-09 22:21:53 +0200

    Initial DB Commit adding App, Schema, Content tables

---
+++ src/Hless.Data.DB/Content.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Content]
+(
+	[ContentId] BIGINT NOT NULL PRIMARY KEY, 
+    [SchemaId] BIGINT NOT NULL, 
+    [Content] NVARCHAR(MAX) NOT NULL, 
+    [Draft] NVARCHAR(MAX) NULL, 
+    [CreatedBy] BIGINT NOT NULL, 
+    [CreatedAt] DATETIMEOFFSET NOT NULL, 
+    [LastModified] DATETIMEOFFSET NULL, 
+    [FirstPublished] DATETIMEOFFSET NULL, 
+    [LastPublished] DATETIMEOFFSET NULL
+)
