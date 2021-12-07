1f0a577ffa333906148136312578b86d998bc1b2
De-Hann <dehannduplessis@gmail.com>
2021-12-07 16:34:50 +0200

    Started Content

---
+++ src/Hless.Api/Controllers/ContentController.cs
@@ -0,0 +1,27 @@
+﻿using Hless.Common.Repositories;
+using Hless.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hless.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ContentController : BaseController
+    {
+        private IContentRepository _reposistory;
+        public ContentController(IContentRepository repository)
+        {
+            _reposistory = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Content>> GetContentsAsync()
+        {
+            return await _reposistory.GetContentsAsync();
+        }
+    }
+}
---
+++ src/Hless.Common/Repositories/IContentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Hless.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hless.Common.Repositories
+{
+    public interface IContentRepository
+    {
+        Task<IEnumerable<Content>> GetContentsAsync();
+        Task<IEnumerable<Content>> GetContentsAsync(long schemaId);
+        Task<Content> GetContentAsync(long contentId);
+        Task<Content> CreateContentAsync(Content content);
+        Task<bool> UpdateContentAsync(Content content);
+        Task<bool> PublishContentAsync(long contentId);
+        Task<bool> DeleteContentAsync(long contentId);
+    }
+}
